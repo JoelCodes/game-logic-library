0437f3184f20cc90e10b13d33177c6ce6f2d6fbf
Joel Shinness <joel@lighthouselabs.ca>
2023-01-16 09:22:29 -0800

    Game/tictactoe undo (#3)
    * Updates package json to reflect new repo
    
    * Adds undo
    
    * Adds skips
    
    * Refactors main tic tac toe for skips, undos, and future added dimensions

--- src/tic-tac-toe/index.test.ts
+++ src/tic-tac-toe/index.test.ts
@@ -1,27 +1,36 @@
-import { initialState, TicTacToeAction, TicTacToeReducer } from '.';
+import {
+  initialState,
+  TicTacToeAction,
+  TicTacToeReducer,
+} from '.';
 
 describe('Tic Tac Toe', () => {
+  const afterFourMoves = [0, 3, 1, 4].map((position) => ({ type: 'MOVE', position }) as TicTacToeAction).reduce(TicTacToeReducer, initialState);
+  const afterGameOver = TicTacToeReducer(afterFourMoves, { type: 'MOVE', position: 2 });
+
   describe('MOVE', () => {
     it('Creates a move, switching the turn', () => {
       const afterFirstMove = TicTacToeReducer(initialState, { type: 'MOVE', position: 0 });
       expect(afterFirstMove).toEqual({
         board: ['X', ...new Array(8).fill(undefined)],
         turn: 'O',
-        gameOver: false,
+        moves: [0],
       });
       const afterSecondMove = TicTacToeReducer(afterFirstMove, { type: 'MOVE', position: 1 });
       expect(afterSecondMove).toEqual({
         board: ['X', 'O', ...new Array(7).fill(undefined)],
         turn: 'X',
-        gameOver: false,
+        moves: [0, 1],
       });
     });
     it('Finds a finished game', () => {
       function hasAWinOn5(positions:number[]) {
         const moves = positions.map((position) => ({ type: 'MOVE', position }) as TicTacToeAction);
-        const afterFourMoves = moves.slice(0, 4).reduce(TicTacToeReducer, initialState);
-        expect(afterFourMoves.gameOver).toBe(false);
-        expect(TicTacToeReducer(afterFourMoves, moves[4]).gameOver).toBe(true);
+        const afterFirstFourMoves = moves.slice(0, 4)
+          .reduce(TicTacToeReducer, initialState);
+        const wins = positions.filter((x, i) => i % 2 === 0);
+        expect(afterFirstFourMoves.gameOver).toBeUndefined();
+        expect(TicTacToeReducer(afterFirstFourMoves, moves[4]).gameOver).toEqual(wins);
       }
 
       // Rows
@@ -45,20 +54,42 @@ describe('Tic Tac Toe', () => {
     });
     it('ignores moves on spots not available on the board', () => {
       const afterFirstMove = TicTacToeReducer(initialState, { type: 'MOVE', position: 0 });
-      expect(TicTacToeReducer(afterFirstMove, { type: 'MOVE', position: -1 } as any as TicTacToeAction)).toEqual(afterFirstMove);
-      expect(TicTacToeReducer(afterFirstMove, { type: 'MOVE', position: 9 } as any as TicTacToeAction)).toEqual(afterFirstMove);
-      expect(TicTacToeReducer(afterFirstMove, { type: 'MOVE', position: 3.5 } as any as TicTacToeAction)).toEqual(afterFirstMove);
+      expect(TicTacToeReducer(afterFirstMove, { type: 'MOVE', position: -1 })).toEqual(afterFirstMove);
+      expect(TicTacToeReducer(afterFirstMove, { type: 'MOVE', position: 9 })).toEqual(afterFirstMove);
+      expect(TicTacToeReducer(afterFirstMove, { type: 'MOVE', position: 3.5 })).toEqual(afterFirstMove);
     });
     it('ignores moves on a finished match', () => {
       const afterWin = [0, 3, 1, 4, 2].map((position) => ({ type: 'MOVE', position }) as TicTacToeAction).reduce(TicTacToeReducer, initialState);
       expect(TicTacToeReducer(afterWin, { type: 'MOVE', position: 5 })).toEqual(afterWin);
     });
   });
-
+  describe('UNDO', () => {
+    it('does nothing to the initial state', () => {
+      expect(TicTacToeReducer(initialState, { type: 'UNDO' })).toEqual(initialState);
+    });
+    it('removes the last mark, pops the last position, and switches back turns', () => {
+      const afterOneMove = TicTacToeReducer(initialState, { type: 'MOVE', position: 0 });
+      const afterTwoMoves = TicTacToeReducer(afterOneMove, { type: 'MOVE', position: 1 });
+      expect(TicTacToeReducer(afterOneMove, { type: 'UNDO' })).toEqual(initialState);
+      expect(TicTacToeReducer(afterTwoMoves, { type: 'UNDO' })).toEqual(afterOneMove);
+    });
+    it('removes a gameOver condition if the game was over', () => {
+      expect(TicTacToeReducer(afterGameOver, { type: 'UNDO' })).toEqual(afterFourMoves);
+    });
+  });
+  describe('SKIP', () => {
+    it('ignores a finished game', () => {
+      expect(TicTacToeReducer(afterGameOver, { type: 'SKIP' })).toEqual(afterGameOver);
+    });
+    it('switches turns', () => {
+      const afterSkip = TicTacToeReducer(initialState, { type: 'SKIP' });
+      expect(afterSkip).toEqual({ ...initialState, turn: 'O' });
+      expect(TicTacToeReducer(afterSkip, { type: 'SKIP' })).toEqual(initialState);
+    });
+  });
   describe('RESET', () => {
-    it('Returns the initial state', () => {
-      const result = TicTacToeReducer(initialState, { type: 'RESET' });
-      expect(result).toEqual(initialState);
+    it('returns the initial undo state', () => {
+      expect(TicTacToeReducer(afterGameOver, { type: 'RESET' })).toEqual(initialState);
     });
   });
 });
--- src/tic-tac-toe/index.ts
+++ src/tic-tac-toe/index.ts
@@ -1,59 +1,96 @@
 export type TicTacToeMark = 'X' | 'O';
 export type TicTacToeCell = TicTacToeMark | undefined;
 
-export type TicTacToeBoard = [
-  TicTacToeCell, TicTacToeCell, TicTacToeCell,
-  TicTacToeCell, TicTacToeCell, TicTacToeCell,
-  TicTacToeCell, TicTacToeCell, TicTacToeCell,
-]
+export type TicTacToeBoard = TicTacToeCell[];
+
 export type TicTacToeState = {
   board: TicTacToeBoard,
   turn: TicTacToeMark,
-  gameOver: boolean
+  gameOver?: [number, number, number],
+  moves: number[]
 };
 
 export const initialState: Readonly<TicTacToeState> = Object.freeze({
-  board: Object.freeze([
-    undefined, undefined, undefined,
-    undefined, undefined, undefined,
-    undefined, undefined, undefined,
-  ]) as TicTacToeBoard,
+  board: Object.freeze([...new Array(9)]) as TicTacToeBoard,
   turn: 'X',
-  gameOver: false,
+  moves: [],
 });
-export type TicTacToeAction = {type: 'RESET'} | {type: 'MOVE', position: 0|1|2|3|4|5|6|7|8};
 
-const lines:[number, number, number][] = [
-  [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-  [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-  [0, 4, 8], [2, 4, 6], // Diagonals
-];
-function isGameOver(board:TicTacToeBoard):boolean {
-  return lines.some(([a, b, c]) => board[a] !== undefined
+export type TicTacToeAction = {type: 'RESET'} | {type: 'MOVE', position: number} | { type: 'UNDO' } | {type: 'SKIP'};
+
+function* makeFace(
+  offset:number,
+  small:number,
+  large:number,
+):IterableIterator<[number, number, number]> {
+  function makeRow(start:number, interval:number):[number, number, number] {
+    return [start, start + interval, start + interval + interval];
+  }
+  yield makeRow(offset, small);
+  yield makeRow(large + offset, small);
+  yield makeRow(large + large + offset, small);
+  yield makeRow(offset, large);
+  yield makeRow(small + offset, large);
+  yield makeRow(small + small + offset, large);
+  yield makeRow(offset, small + large);
+  yield makeRow(offset + small + small, large - small);
+}
+const lines2d:[number, number, number][] = [...makeFace(0, 1, 3)];
+
+function makeTicTacToeReducer(_initialState:TicTacToeState, _lines:[number, number, number][]) {
+  function isGameOver(board:TicTacToeBoard):[number, number, number]|undefined {
+    return _lines.find(([a, b, c]) => board[a] !== undefined
     && board[a] === board[b]
     && board[a] === board[c]);
+  }
+  return function reducer(state:TicTacToeState, action:TicTacToeAction):TicTacToeState {
+    switch (action.type) {
+      case 'UNDO': {
+        const newMoves = [...state.moves];
+        const lastMove = newMoves.pop();
+        if (lastMove === undefined) return state;
+        const newBoard = [...state.board];
+        newBoard[lastMove] = undefined;
+        const newState = {
+          ...state,
+          board: newBoard as TicTacToeBoard,
+          moves: newMoves,
+          turn: state.turn === 'O' ? 'X' : 'O',
+        };
+        delete newState.gameOver;
+        return newState as TicTacToeState;
+      }
+      case 'SKIP': {
+        if (state.gameOver) return state;
+        return {
+          ...state,
+          turn: state.turn === 'X' ? 'O' : 'X',
+        };
+      }
+      case 'MOVE': {
+        if (state.gameOver) return state;
+        if (
+          action.position < 0
+            || action.position >= state.board.length
+            || Math.floor(action.position) !== action.position
+        ) return state;
+        if (state.board[action.position] !== undefined) return state;
+        const newBoard = [...state.board] as TicTacToeBoard;
+        newBoard[action.position] = state.turn;
+        const newTurn:TicTacToeMark = state.turn === 'O' ? 'X' : 'O';
+        return {
+          ...state,
+          board: newBoard,
+          turn: newTurn,
+          moves: [...state.moves, action.position],
+          gameOver: isGameOver(newBoard),
+        };
+      }
+      case 'RESET': return _initialState;
+
+      default: return state;
+    }
+  };
 }
 
-export function TicTacToeReducer(state:TicTacToeState, action:TicTacToeAction):TicTacToeState {
-  switch (action.type) {
-    case 'RESET': return initialState;
-    case 'MOVE': {
-      if (state.gameOver) return state;
-      if (
-        action.position < 0
-        || action.position >= state.board.length
-        || Math.floor(action.position) !== action.position
-      ) return state;
-      if (state.board[action.position] !== undefined) return state;
-      const newBoard = [...state.board] as TicTacToeState['board'];
-      newBoard[action.position] = state.turn;
-      const newTurn:TicTacToeMark = state.turn === 'O' ? 'X' : 'O';
-      return {
-        board: newBoard,
-        turn: newTurn,
-        gameOver: isGameOver(newBoard),
-      };
-    }
-    default: { return state; }
-  }
-}
+export const TicTacToeReducer = makeTicTacToeReducer(initialState, lines2d);
